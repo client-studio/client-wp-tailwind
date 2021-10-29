361cf62f23e9fbd0445ef1f3945639db8800e55d
client-studio <hei@client.fi>
2021-10-29 12:49:13 +0300

    404

--- 404.php
+++ 404.php
@@ -10,13 +10,13 @@
 <body class="antialiased bg-black">
 	<div class="md:flex min-h-screen">
 		<div class="w-full md:w-1/2 md:mx-auto flex items-center justify-center">
-			<div class="m-8 text-center">
-				<div class="text-white text-5xl md:text-15xl mb-3">404</div>
-				<p class="text-gray-100 text-2xl md:text-3xl font-light mb-8"><?php _e( 'Sorry, the page you are looking for could not be found.', 'tailpress' ); ?></p>
-				<a href="<?php echo get_bloginfo( 'url' ); ?>" class="bg-primary px-4 py-3 px-6 rounded text-white">
-					<?php _e( 'Frontpage', 'tailpress' ); ?>
+			<article class="m-8 text-center">
+				<div class="text-white text-5xl md:text-8xl mb-3">404</div>
+				<p class="text-gray-100 text-2xl mb-8"><?php _e( 'Sorry, you are in the wrong place.', 'client' ); ?></p>
+				<a href="<?php echo get_bloginfo( 'url' ); ?>" class="btn">
+					<?php _e( 'Frontpage', 'client' ); ?>
 				</a>
-			</div>
+			</article>
 		</div>
 	</div>
 </body>
